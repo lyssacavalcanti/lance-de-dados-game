c038187a463f3eae34c44b7cdf66bdb9ddb74a6e
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-26 23:40:18 -0300

    Add emojis in HTML sheet and news variables on JS.

--- script.js
+++ script.js
@@ -4,8 +4,12 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold')
 
 //Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+diceEl.classList.add('hidden'); 
+ 
