a890c6c2203346eb9a88d357466d30997788ae84
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-27 00:15:45 -0300

    Add emojis in HTML sheet and news variables on JS.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -16,7 +19,7 @@ diceEl.classList.add('hidden');
 let currentScore = 0;
 
 // Rolling dice functionality
-btnHold.Roll.addEvent AudioListener('click', function (){
+btnRoll.addEventListener('click', function (){
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() *6) + 1;
     console.log(dice);
@@ -29,6 +32,7 @@ btnHold.Roll.addEvent AudioListener('click', function (){
     if(dice!==1){
         // Add dice to current score
         currentScore += dice;
+        current0El.textContent = currentScore; //Change later
     } else {
         //Switch to next player
     }
